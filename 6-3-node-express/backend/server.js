e2cc0013156e5b4fc980e70f7765098d146e4d16
kawthar1235 <s202168810@kfupm.edu.sa>
2026-04-06 13:01:38 +0300

    task1

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,7 @@
 // TODO 1: Import and Create express app instance
+import express from "express";
+const app = express();
 
 // TODO 1: Define server port
+const PORT = 3000;
 
-
-// TODO 4: Import and Apply CORS middleware
-
-
-// TODO 5: Import and apply Morgan middleware
-
-
-// TODO 6.1: Create root route "/"
-
-
-// TODO 6.2: Create "/api/quote" route
-
-
-// TODO 7: Start server using app.listen
